11ad037c535d701fcf0a9d474fff3f23d1df490b
Wong <wz_jxnu@163.com>
2018-07-28 01:02:35 +0800

    Update 60_Humidity_temperature.py

--- rasp-master/60_Humidity_temperature.py
+++ rasp-master/60_Humidity_temperature.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python
+#!-*- coding:utf8 -*-
+
+import requests
+import time
+import json
+import random
+import Adafruit_DHT
+
+ts = int(time.time())
+sensor1 = Adafruit_DHT.DHT11
+humidity1, temperature1 = Adafruit_DHT.read_retry(sensor1, 4)
+#print humidity1,temperature1
+
+payload = [
+            {
+                        "endpoint": "Master",
+                        "metric": "temperature",
+                        "timestamp": ts,
+                        "step": 60,
+                        "value": temperature1,
+                        "counterType": "GAUGE",
+                        "tags": "facility=raspberry",
+            },
+
+            {
+                        "endpoint": "Master",
+                        "metric": "humidity",
+                        "timestamp": ts,
+                        "step": 60,
+                        "value": humidity1,
+                        "counterType": "GAUGE",
+                        "tags": "facility=raspberry",
+            },
+        ]
+
+r = requests.post("http://127.0.0.1:1988/v1/push", data=json.dumps(payload))
+print r
